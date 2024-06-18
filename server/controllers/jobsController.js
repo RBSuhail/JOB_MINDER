a00eea2a1b7f2eea4a0cd1bd1107da778ac7a31f
Suhail Ahmad <rbsuhail@gmail.com>
2024-06-18 10:15:44 +0530

    controllers for jobs and authentication has been added

---
+++ server/controllers/jobsController.js
@@ -0,0 +1,46 @@
+import Job from "../models/Job.js"
+import { StatusCodes } from "http-status-codes"
+import {BadRequestError , NotFoundError} from '../errors/index.js'
+
+
+
+
+
+const createJob = async(req, res)=>{
+    const {position,company} = req.body
+
+    if(!position || !company){
+        throw new BadRequestError('Please provide all values');
+    }
+
+    req.body.createdBy = req.user.userId
+
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job });
+}
+
+
+const getAllJobs = async(req, res)=>{
+   const jobs = await Job.find({createdBy:req.user.userId})
+   res.status(StatusCodes.OK).json({jobs,totalJobs:jobs.length, numOfPages:1})
+}
+
+
+const updateJob = async(req, res)=>{
+    res.send('update Job')
+}
+
+
+const deleteJob = async(req, res)=>{
+    res.send('delete Job')
+}
+
+
+const showStats= async(req, res)=>{
+    res.send('show stats')
+}
+
+export {createJob, getAllJobs, updateJob, deleteJob, showStats}
+
+
+
