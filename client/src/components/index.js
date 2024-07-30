a90d75966c4cfc04789e6a6ed0ad74eee76ab549
RB SUHAIL <114150506+RBSuhail@users.noreply.github.com>
2024-07-30 17:21:32 +0530

    Frontent Part For Job Minder Has Been  Uploaded

---
+++ client/src/components/index.js
@@ -0,0 +1,22 @@
+import Alert from './Alert'
+import BigSidebar from './BigSidebar'
+import FormRow from './FormRow'
+import Logo from './Logo'
+import Navbar from './Navbar'
+import SmallSidebar from './SmallSidebar'
+import FormRowSelect from './FormRowSelect'
+import SearchContainer from './SearchContainer'
+import JobsContainer from './JobsContainer'
+
+
+export {
+    Logo,
+    FormRow,
+    Alert,
+    Navbar,
+    BigSidebar,
+    SmallSidebar,
+    FormRowSelect,
+    SearchContainer,
+    JobsContainer,
+}
